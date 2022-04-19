251e1794a5ab80f914486e01a558bf1adb2ebc33
Samuel-Webster-178 <99287154+Samuel-Webster-178@users.noreply.github.com>
2022-04-19 13:52:06 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by Samuel Webster
+# Created on March 2022
+# constants
+
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
