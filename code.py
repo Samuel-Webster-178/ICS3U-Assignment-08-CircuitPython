d6fca88fdabe3af21f13329bd01a8cdf45f3bba6
Samuel-Webster-178 <99287154+Samuel-Webster-178@users.noreply.github.com>
2022-04-27 14:15:12 -0400

    Update code.py

--- code.py
+++ code.py
@@ -5,8 +5,102 @@
 # "Space Aliens" program on PyBadge
 
 import constants
+import random
 import stage
+import time
 import ugame
+
+
+def splash_scene():
+    # I am the splash scene
+    coin_sound = open("coin.wav", "rb")
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y)
+    
+    # used this program to split the image into tile: 
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+
+    game = stage.Stage(ugame.display, constants.FPS)
+    game.layers = [background]
+    game.render_block()
+
+    # repeat forever game loop
+    while True:
+        time.sleep(2.0)
+        menu_scene()
+
+
+def menu_scene():
+    # I am the menu scene
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    text = []
+    text1 = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
+    text1.move(20, 10)
+    text1.text("MT Game Studios")
+    text.append(text1)
+
+    text2 = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
+    text2.move(40, 110)
+    text2.text("PRESS START")
+    text.append(text2)
+
+    background = stage.Grid(
+        image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y
+    )
+
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers of all sprites, items show up in order
+    game.layers = text + [background]
+    # render all sprites
+    #    likely you only render the background once per game scene
+    game.render_block()
+
+    # repeat forever game loop
+    while True:
+        # get input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_START != 0:
+            game_scene()
+
+        # update game logic
+        game.tick()
 
 
 def game_scene():
@@ -19,20 +113,25 @@ def game_scene():
     start_button = constants.button_state["button_up"]
     select_button = constants.button_state["button_up"]
 
-    pew_sound = open("pew.wav", "rb")
+    pew_sound = open('pew.wav', 'rb')
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
 
-    background = stage.Grid(image_bank_background, 10, 8)
+    # random background
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_X, constants.SCREEN_Y
+    )
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 3)
+            background.tile(x_location, y_location, tile_picked)
+
     ship = stage.Sprite(
         image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
     )
     alien = stage.Sprite(
-        image_bank_sprites,
-        9,
-        int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
-        16,
+        image_bank_sprites, 9, int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2), 16
     )
 
     game = stage.Stage(ugame.display, 60)
@@ -69,9 +168,9 @@ def game_scene():
         if a_button == constants.button_state["button_just_pressed"]:
             sound.play(pew_sound)
 
-        game.render_sprites([ship])
+        game.render_sprites([ship] + [alien])
         game.tick()
 
 
 if __name__ == "__main__":
-    game_scene()
+    splash_scene()
